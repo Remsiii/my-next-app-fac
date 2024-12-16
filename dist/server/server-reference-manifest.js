eddcec1fc0895376ca4ea9b42ece328dad59042a
Daniel Remetan <daniel.remetan@kwb.at>
2024-12-16 15:24:33 +0100

    Initial commit

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mRiLKR0KZ+5oorfCYGrgKbw+w+Tn6ns6tztnP9EeezU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
